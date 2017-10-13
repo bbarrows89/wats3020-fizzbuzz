66aab12fe21261575f9b91ec038065a09101cc42
Bryan Barrows <bryan.barrows@seattlecolleges.edu>
2017-10-12 19:55:27 -0700

    working through assignment

--- js/main.js
+++ js/main.js
@@ -9,23 +9,36 @@ let isInteger,
 // TODO: Initialize a variable `isInteger` to use as a control value for the
 // `while` loop. Set the initial value to `false`.
 
+isInteger = false;
+
 // TODO: Create a `while` loop so user will be prompted to enter a number until
 // they enter a good number. This loop should be controlled by a conditional
 // expression that looks at the value of `isSafeInteger`. When `isSafeInteger`
 // becomes `true`, the `while` loop should stop looping.
 
-// TODO: Inside `while` loop prompt the user for the `maxNumber` value.
-
-// TODO: Inside the `while` loop, use a conditional to verify if the `maxNumber`
-// is a suitable integer. (It should be an integer greater than zero.)
-
-// TODO: If the value of `maxNumber` is suitable, then change the value of
-// `isInteger` so the `while` loop stops looping.
+while (isInteger = false) {
+  maxNumber = prompt("Please enter a maximum number for FizzBuzz! (use whole numbers)");
+  maxNumber = parseInt(maxNumber);
+  if (Number.isSafeInteger(maxNumber) && maxNumber > 0) {
+    isInteger = true;
+  }
+}
 
 // TODO: Initialize the `fbResults` variable to an empty Array
-
+fbResults = [];
 // TODO: Create a `for` loop that will execute the `maxNumber` of times.
+for (let i=1; i <= maxNumber; i++) {
 
+  if (i % 15 == 0) {
+    fbResults.push('FizzBuzz');
+  } else if (i % 3 == 0) {
+    fbResults.push('Fizz');
+  } else if (i % 5 == 0) {
+    fbResults.push('Buzz');
+  } else {
+    fbResults.push(i);
+  }
+}
 // TODO: Create logic inside the `for` loop to calculate FizzBuzz.  This will
 // require the use of several conditional statements that use the `%` operator.
 // Store the results of this logic in an array called `fbResults`.
